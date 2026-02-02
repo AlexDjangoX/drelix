b436e051ddfa87dc36cd8d9fe47fde490ac98fec
AlexDjangoX <alexmonk17@gmail.com>
2026-02-02 19:01:37 +0100

    SEO - inline CSS

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,34 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "*.convex.cloud",
+        protocol: 'https',
+        hostname: '*.convex.cloud',
       },
     ],
+  },
+  experimental: {
+    inlineCss: true,
   },
   async headers() {
     return [
       {
-        source: "/",
+        source: '/',
         headers: [
           {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
           },
         ],
       },
       {
-        source: "/products/:slug",
+        source: '/products/:slug',
         headers: [
           {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
           },
         ],
       },
